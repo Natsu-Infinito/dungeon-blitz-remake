b6d9784fbdc28b4f78365ab4440b9771076ded19
Natsu-Infinito <108439839+Natsu-Infinito@users.noreply.github.com>
2022-08-18 21:02:36 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1660866419,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player_box-sheet0.png",
+		"images/tile_solid.png",
+		"images/attackarea-sheet0.png",
+		"images/cursors-sheet0.png",
+		"images/obj_projectiledistance-sheet0.png",
+		"images/effect_attackmelee-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
